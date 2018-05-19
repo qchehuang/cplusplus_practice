12524e5077c398370730c164d716e68f88f982b3
qchehuang <hhuan10@126.com>
2018-05-19 18:16:13 +0800

    add first hello application via Xcode

---
+++ helloword/helloword/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloword
+//
+//  Created by hehuang on 18/5/19.
+//  Copyright © 2018年 hehuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
